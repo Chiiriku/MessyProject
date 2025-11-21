50e082d98051f111fe098ea3ee7c170a3a716b2e
Chiiriku <224570909+Chiiriku@users.noreply.github.com>
2025-11-21 03:31:59 +0100

    Korrekthed:
    Har skriftet rækkefølge på int og string, så den var lig constructoren.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main
 {
     public static void main(String[] args)
     {
-        User u1 = new User("Bob", 10);
-        User u2 = new User("Alice", 20);
-        User u3 = new User("Carl", 30);
+        User u1 = new User(10, "Bob");
+        User u2 = new User(20, "Alice");
+        User u3 = new User(30, "Carl");
 
         UserList list = new UserList();
         list.add(u1);
