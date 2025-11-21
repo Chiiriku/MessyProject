d17c1c25d939e6fb0c867f95e64efafd67ced962
Chiiriku <224570909+Chiiriku@users.noreply.github.com>
2025-11-21 03:50:50 +0100

    Forståelighed:
    Kommentar er fjernet og rettet.
    
    Korrekthed:
    
    Har fjernet -1 i for loopen. Loopen ender med ikke at tage den sidste bruger med.
    
    Har lukket filen med bw.close()

--- src/Persister.java
+++ src/Persister.java
@@ -5,13 +5,15 @@ public class Persister
 {
     public static void s(ArrayList<User> u) throws IOException
     {
-        File file = new File("users.txt"); // Creates file
-        FileWriter fw = new FileWriter(file); // Create filewriter
+        File file = new File("users.txt"); // Creates file object
+        FileWriter fw = new FileWriter(file); // Creates file
         BufferedWriter bw = new BufferedWriter(fw); // Create BufferedWriter
-        for(int x=0;x<u.size()-1;x++) // Create a loop
-        { // Nicely formatted block
+        for(int x=0;x<= u.size();x++) // Create a loops
+        {
             bw.write(u.get(x).c_nam); // Write first thing
-            bw.write(u.get(x).geti()); // Write second thing
-        } // Ending parenthesis
+            bw.write(u.get(x).geti()); // Write second thin
+
+          bw.close();
+        }
     }
 }
